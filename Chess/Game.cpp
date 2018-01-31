4df73850684c492728f666ecb8c23278f54db7b1
Nikitaenterprise <prof.frimen@mail.ru>
2018-01-31 23:05:28 +0300

    Commit #9.3
    SetUp figures done

--- Chess/Game.cpp
+++ Chess/Game.cpp
@@ -6,35 +6,6 @@ Game::Game() : _window(sf::VideoMode(WIDTH, HEIGHT),"Game")
 	class
 	{
 	public:
-		void operator()(sf::Vector2i &pos, int id)
-		{
-			for (int i = 0; i < 8; i++)
-			{
-				for (int j = 0; j < 8; j++)
-				{
-					int n = board[i][j];
-					if (n == 0) continue;
-					if (n == id)
-					{
-						pos.y = abs(n);
-						pos.x = n > 0 ? 1 : 0;
-						std::cout << "id = " << id  << "\ti = " << i << "\tj = " << j << "\tn = " << n << "\tpos.x = " << pos.x << "\tpos.y = " << pos.y << std::endl;
-						board[i][j] = 9;
-						break;
-					}
-				}
-			}
-		}
-		int board[8][8] = {
-								-1,-2,-3,-4,-5,-3,-2,-1,
-								-6,-6,-6,-6,-6,-6,-6,-6,
-								0, 0, 0, 0, 0, 0, 0, 0,
-								0, 0, 0, 0, 0, 0, 0, 0,
-								0, 0, 0, 0, 0, 0, 0, 0,
-								0, 0, 0, 0, 0, 0, 0, 0,
-								6, 6, 6, 6, 6, 6, 6, 6,
-								1, 2, 3, 4, 5, 3, 2, 1 
-														};
 		char *board2[8][8] =
 		{
 		{ "castle", "knight", "bishop", "queen", "king", "bishop", "knight", "castle" },
@@ -44,24 +15,43 @@ Game::Game() : _window(sf::VideoMode(WIDTH, HEIGHT),"Game")
 		{ "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty" },
 		{ "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty" },
 		{ "pawn" , "pawn" , "pawn" , "pawn" , "pawn" , "pawn" , "pawn" , "pawn" },
-		{ "castle", "knight", "bishop", "queen", "king", "bishop", "knight", "castle" },
+		{ "castle", "knight", "bishop", "king", "queen", "bishop", "knight", "castle" },
 		};
-		void operator()(sf::Vector2i &pos, std::vector<std::string> *name)
+		void operator()(sf::Vector2i &pos, std::string &name)
 		{
 			for (int i = 0; i < 8; i++)
 			{
 				for (int j = 0; j < 8; j++)
 				{
-					std::cout << board2[i][j] << "\t";
+					if (board2[i][j] == "empty") continue;
+					if (board2[i][j] == name)
+					{
+						pos.x = j;
+						pos.y = i;
+						//std::cout << "\ti = " << i << "\tj = " << j << "\tboard2[i][j] = " << board2[i][j] << "\tpos.x = " << pos.x << "\tpos.y = " << pos.y << std::endl;
+						board2[i][j] = "zero";
+						goto breakLoop;
+					}
 				}
 			}
+		breakLoop:
+			return;
+		}
+		void setFigure(std::vector<Figure*> &_figures, std::string &name, std::string &color, sf::Vector2i &pos, int &k)
+		{
+			_figures.push_back(new Figure(name, color));
+			(*this)(pos, name);
+			std::cout << pos.x << "\t" << pos.y << "\t" << color << "\t" << name << std::endl;
+			if (k == 9 || k == 10) this->show();
+			_figures[k]->setPosition(sf::Vector2f(800 / 8 * pos.x, 800 / 8 * pos.y));
+			k++;
 		}
 		void show()
 		{
 			std::cout << std::endl;
 			for (int i = 0; i < 8; i++) {
 				for (int j = 0; j < 8; j++) {
-					std::cout << board[i][j] << "\t";
+					std::cout << board2[i][j] << "\t";
 				}
 				std::cout<<std::endl;
 			}
@@ -70,7 +60,7 @@ Game::Game() : _window(sf::VideoMode(WIDTH, HEIGHT),"Game")
 
 	int k = 0;
 	sf::Vector2i pos;
-	enum figuresNameEnum { pawn = 6, castle = 1, knight = 2, bishop = 3, queen = 4, king = 5};
+	//enum figuresNameEnum { pawn = 6, castle = 1, knight = 2, bishop = 3, queen = 4, king = 5};
 	std::vector<std::string> figuresName = { "pawn", "castle", "knight", "bishop", "queen", "king" };
 	std::vector<std::string> figuresColor = { "white", "black" };
 	for (auto it1 = figuresColor.begin(); it1 != figuresColor.end(); it1++)
@@ -78,58 +68,14 @@ Game::Game() : _window(sf::VideoMode(WIDTH, HEIGHT),"Game")
 		for (auto it2 = figuresName.begin(); it2 != figuresName.end(); it2++)
 		{
 			if (*it2 == "pawn")
-			{
-				for (int l = 0; l < 8; l++)
-				{
-					_figures.push_back(new Figure((*it2), (*it1)));
-					getStartCoord(pos, figuresNameEnum(pawn));
-					_figures[k]->setPosition(sf::Vector2f(l * 800 / 8 * pos.x, 800 / 8 * pos.y));
-					k++;
-					//getStartCoord(pos, );
-				}
-			}
-			else if (*it2 == "king")
-			{
-				_figures.push_back(new Figure((*it2), (*it1)));
-				getStartCoord(pos, figuresNameEnum(king));
-				_figures[k]->setPosition(sf::Vector2f(800 / 8 * pos.y, 800 / 8 * pos.x));
-				k++;
-			}
-			else if (*it2 == "queen")
-			{
-				_figures.push_back(new Figure((*it2), (*it1)));
-				getStartCoord(pos, figuresNameEnum(queen));
-				_figures[k]->setPosition(sf::Vector2f(800 / 8 * pos.y, 800 / 8 * pos.x));
-				k++;
-			}
-			else
-			{
-				for (int l = 0; l < 2; l++)	_figures.push_back(new Figure((*it2), (*it1)));
-				getStartCoord(pos, figuresNameEnum(knight));
-				_figures[k]->setPosition(sf::Vector2f(800 / 8 * pos.y, 800 / 8 * pos.x));
-				k++;
-			}
+				for (int l = 0; l < 8; l++)	getStartCoord.setFigure(_figures, *it2, *it1, pos, k);
+			else if (*it2 == "king") getStartCoord.setFigure(_figures, *it2, *it1, pos, k);
+			else if (*it2 == "queen") getStartCoord.setFigure(_figures, *it2, *it1, pos, k);
+			else if (*it2 == "castle" || *it2 == "knight" || *it2 == "bishop")
+				for (int l = 0; l < 2; l++)	getStartCoord.setFigure(_figures, *it2, *it1, pos, k);
 		}
 	}
-	getStartCoord.show();
-	//for (int i = 0; i < 8; i++)
-	//{
-	//	for (int j = 0; j < 8; j++)
-	//	{
-	//		int n = board[i][j];
-	//		if (n == 0) continue;
-	//		int x = abs(n) - 1;
-	//		int y = n > 0 ? 1 : 0;
-	//		std::cout << x << "\t" << y << "\t" << n << std::endl;
-	//		/*for (auto it = _figures.begin(); it != _figures.end(); it++)
-	//		{
-	//			(*it)->setPosition(sf::Vector2f(800/8*i, 800/8*j));
-	//		}*/
-	//		_figures[k]->setPosition(sf::Vector2f(800 / 8 * j, 800 / 8 * i));
-	//		k++;
-	//	}
-	//}
-	
+	getStartCoord.show();	
 }
 
 Game::~Game()
